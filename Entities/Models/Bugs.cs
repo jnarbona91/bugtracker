526a90bbb4c9626d0321ae7c06ad490c5e92ee28
jnarbona91 <jnarbona91@gmail.com>
2019-12-10 12:45:08 -0800

    added entity framework and models for db

---
+++ Entities/Models/Bugs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Models
+{
+    public class Bugs
+    {
+        public Guid BugsId { get; set; }
+
+        [Required(ErrorMessage = "Date is required")]
+        public DateTime DateOfBug { get; set; }
+
+        [Required(ErrorMessage = "Bug details required")]
+        public string BugDetails { get; set; }
+
+        [ForeignKey(nameof(Project))]
+        public Guid ProjectId { get; set; }
+        public Project Project { get; set; }
+    }
+}
---
+++ Entities/Models/Project.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Models
+{
+    [Table("project")]
+    public class Project
+    {
+        public Guid ProjectId { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Date is required")]
+        public DateTime DateStarted { get; set; }
+
+        [Required(ErrorMessage = "Project details required")]
+        public string ProjectDetails { get; set; }
+
+        public ICollection<Bugs> Bugs { get; set; }
+    }
+}
