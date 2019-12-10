61781ecf89f5cd880ad84ba4297e632929fa68bc
jnarbona91 <jnarbona91@gmail.com>
2019-12-10 10:58:40 -0800

    First Commit, CORS set up

---
+++ BugTracker/Extensions/ServicesExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace BugTracker.Extensions
+{
+    public static class ServicesExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(Base64FormattingOptions =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+            });
+        }
+
+        public static void ConfigureIISIntergration(this IServiceCollection services)
+        {
+            services.Configure<IISOptions>(options =>
+            {
+
+            });
+        }
+    }
+}
